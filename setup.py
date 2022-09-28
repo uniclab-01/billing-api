ae19bf618e4f255261e293800ac3f29ccbfc93cd
Artem9751 <a.brilon@unic-lab.by>
2022-09-28 10:59:35 +0300

    0.0.9

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='unicboard_billing_sdk',
-    version='0.0.8',
+    version='0.0.9',
     description='Unicboard billing api',
     author='Unic-lab',
     long_description=long_description,
